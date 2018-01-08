355b5d3775ba80d42e40097a2cb0e6a680c49d03
colohr <colohr.code@gmail.com>
2018-01-08 01:10:08 -0600

    wxy update

---
+++ modules/wwi/component/design/css/colors.legend.js
@@ -0,0 +1,25 @@
+/*
+-----------------------------
+Legend:
+	🎨 color
+	◯ version or base
+	# number
+
+	name		-> description
+	-----------------------------
+	🎨-bright	-> more saturation
+	🎨-dim 		-> desaturated
+	🎨-less 	-> least saturation
+	🎨-bold 	-> more saturation dark
+
+	🎨-white 	-> lighter version
+	🎨-light 	-> light version
+	🎨 			-> base color
+	🎨-dark 	-> dark version
+	🎨-black 	-> darker version
+
+	🎨-◯-50 	-> 50% transparency
+	🎨-◯-# 		-> #% transparency
+
+-----------------------------
+*/
